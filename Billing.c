a0f29988daeda98a18c7a990b8b792413271e8d5
Piyush Mehta <me@piyushmehta.com>
2017-11-20 02:29:54 +0530

    Update Billing.c

--- Billing.c
+++ Billing.c
@@ -1,41 +1,31 @@
 //simple project to store frequent passenger information and airlines billing system! (dynamic Pricing) 
 #include<stdio.h>
  #include<stdlib.h>
-
- void input();
- void search();
- void output();
-
 //linked list for storing passenger info
 struct node
 {
 	int info;
 	int frequentFlyID;
 	char name[40];
-	
- struct date{
-	   int month;
-	   int day;
-	   int year;
-	   };
+	char from[10],to[10];
+	struct node *linkReference;
+};
+typedef struct node *Nodeptr;
+//correct till here (TESTED)
 
-  struct account {
-	int number;
-	char name[100];
-	int acct_no;
-	float mobile_no;
-	char street[100];
-	char city[100];
-	char acct_type;
-	float oldbalance;
-	float newbalance;
-	float payment;
-	struct date lastpayment;
-  }customer;
-  int tl,sl,ts;
- void main()
-	{
-	  int i,n;
-	  char ch;
- //TODO: will be added soon
- }
+//gonna check this (Linked List Implementations 
+void insertData(Nodeptr p)
+{
+	printf("Enter the name of the passenger");
+	scanf("%c",p->name);
+	printf("Enter the Frequent flyer ID");
+	scanf("%d",&p->frequentFlyID);
+	printf("Where you are travelling from?");
+	scanf("%c",p->from);
+	printf("Where you are travelling to?");
+	scanf("%c",p->to);
+	
+	
+
+}
+//will be added soon
